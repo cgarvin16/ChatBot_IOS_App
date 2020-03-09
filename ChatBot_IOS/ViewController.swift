b00b965859ba2549376abe8f87393829b551a49f
Carolyn Garvin <ctgarvin@iu.edu>
2020-03-09 19:52:10 -0400

    Made an IOS chatbot by using Cocoapods from Salesforce SDK

--- ChatBot_IOS/ViewController.swift
+++ ChatBot_IOS/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import ServiceChat
 
 class ViewController: UIViewController {
+    let POD = "d.la1-c2-ia5.salesforceliveagent.com"
+    let ORG_ID = "00D6g000008O3dH"
+    let DEPLOYMENT_ID = "5726g000000hNRe"
+    let BUTTON_ID = "5736g000000hOwO"
+    
+    var chatConfig: SCSChatConfiguration?
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      super.viewDidLoad()
+      chatConfig = SCSChatConfiguration(liveAgentPod: POD,
+                                        orgId: ORG_ID,
+                                        deploymentId: DEPLOYMENT_ID,
+                                        buttonId: BUTTON_ID)
     }
 
-
+    @IBAction func chatButtonTapped(_ sender: Any) {
+            ServiceCloud.shared().chatUI.showChat(with: chatConfig!)
+    }
+    
 }
 
